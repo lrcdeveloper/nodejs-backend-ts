c5911beec42dd174ed63277373ca58a42adf243a
leo rigby <lrcdeveloper@gmail.com>
2022-04-26 22:24:15 -0500

    Change port

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,4 @@ app.get('/api', (req: Request, res: Response, next: NextFunction) => {
   res.send('hello world')
 });
 
-app.listen(8000, () => { console.log('Listening on port:', 8000) })
+app.listen(process.env.PORT || 3000, () => { console.log('Listening on port:', process.env.PORT || '3000') })
